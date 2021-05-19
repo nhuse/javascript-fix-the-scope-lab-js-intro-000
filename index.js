d0f845482f1e307f4c7db86eaa3b7e0e0ead05f3
Nathan Huse <nathan.huse@yahoo.com>
2021-05-19 21:01:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,10 +16,9 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
-
   // Feel free to move things around!
   const two = 2
+  return n + two
 }
 
 var funkyFunction = function() {
@@ -30,4 +29,4 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction();
